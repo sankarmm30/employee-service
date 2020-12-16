89ff43733aaf179b347bb58470f67e595b579969
sankarmm30 <sankar.mm30@gmail.com>
2020-12-17 00:48:53 +0100

    Added unit test for EmployeeController

--- src/main/java/com/takeaway/challenge/exception/TakeAwayRuntimeException.java
+++ src/main/java/com/takeaway/challenge/exception/TakeAwayRuntimeException.java
@@ -2,9 +2,6 @@ package com.takeaway.challenge.exception;
 
 public class TakeAwayRuntimeException extends RuntimeException {
 
-    public TakeAwayRuntimeException() {
-    }
-
     public TakeAwayRuntimeException(String message) {
         super(message);
     }
