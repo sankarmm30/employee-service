4e882c62a9de0cca7aa0f4e00cb9fbb5a5fd8bf9
sankarmm30 <sankar.mm30@gmail.com>
2020-12-18 01:25:29 +0100

    Created Dockerfile and docker-compose for supporting containerization

--- src/main/java/com/takeaway/challenge/context/EmployeeEventKafkaProducerContext.java
+++ src/main/java/com/takeaway/challenge/context/EmployeeEventKafkaProducerContext.java
@@ -6,11 +6,9 @@ import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
 import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import io.confluent.kafka.serializers.subject.TopicRecordNameStrategy;
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.config.SslConfigs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -33,14 +31,9 @@ public class EmployeeEventKafkaProducerContext {
 
     private static final Logger LOG = LoggerFactory.getLogger(EmployeeEventKafkaProducerContext.class);
 
-    private static final String BOOTSTRAP_SERVERS_PROP_NAME = "kafka.producer.bootstrap.servers";
-    private static final String SECURITY_ENABLE_PROP_NAME = "kafka.producer.security.enable";
-    private static final String SPECIFIC_AVRO_READER_CONFIG_VALUE = "true";
-    private static final String DEFAULT_SECURITY_PROTOCOL = "SSL";
-    private static final String DEFAULT_SSL_ENABLED_PROTOCOLS = "TLSv1.2,TLSv1.1,TLSv1";
-    private static final String DEFAULT_SSL_TRUSTSTORE_TYPE = "JKS";
-    private static final String DEFAULT_SSL_KEYSTORE_TYPE = "JKS";
+    private static final String KAFKA_PRODUCER_PREFIX = "kafka.producer.";
     private static final String DEFAULT_SCHEMA_REGISTRY_URL = "http://localhost:8081";
+    private static final Boolean SPECIFIC_AVRO_READER_CONFIG_VALUE = true;
 
     @Autowired
     private Environment environment;
@@ -54,27 +47,39 @@ public class EmployeeEventKafkaProducerContext {
     public Map<String, Object> getProducerConfig() {
 
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, environment.getRequiredProperty(BOOTSTRAP_SERVERS_PROP_NAME));
+
+        // bootstrap.servers
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                environment.getRequiredProperty(KAFKA_PRODUCER_PREFIX + ProducerConfig.BOOTSTRAP_SERVERS_CONFIG));
+
+        // key.serializer & value.serializer
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, environment.getProperty(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG, DEFAULT_SCHEMA_REGISTRY_URL));
-        props.put(KafkaAvroDeserializerConfig.AUTO_REGISTER_SCHEMAS, environment.getProperty(KafkaAvroDeserializerConfig.AUTO_REGISTER_SCHEMAS, Boolean.class, false));
-        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, SPECIFIC_AVRO_READER_CONFIG_VALUE);
-        props.put(AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class.getName());
-        props.put(AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY, TopicRecordNameStrategy.class.getName());
 
-        if(environment.getProperty(SECURITY_ENABLE_PROP_NAME, Boolean.class, false)) {
-            props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, environment.getProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, DEFAULT_SECURITY_PROTOCOL));
-            props.put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, environment.getProperty(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, ""));
-            props.put(SslConfigs.SSL_ENABLED_PROTOCOLS_CONFIG, environment.getProperty(SslConfigs.SSL_ENABLED_PROTOCOLS_CONFIG, DEFAULT_SSL_ENABLED_PROTOCOLS));
-            props.put(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, environment.getProperty(SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG, DEFAULT_SSL_TRUSTSTORE_TYPE));
-            props.put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, environment.getProperty(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG));
-            props.put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, environment.getProperty(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG));
-            props.put(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, environment.getProperty(SslConfigs.SSL_KEYSTORE_TYPE_CONFIG, DEFAULT_SSL_KEYSTORE_TYPE));
-            props.put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, environment.getProperty(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG));
-            props.put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, environment.getProperty(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG));
-            props.put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, environment.getProperty(SslConfigs.SSL_KEY_PASSWORD_CONFIG));
-        }
+        // schema.registry.url
+        props.put(KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                environment.getProperty(KAFKA_PRODUCER_PREFIX + KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG,
+                        DEFAULT_SCHEMA_REGISTRY_URL));
+
+        // auto.register.schemas
+        props.put(KafkaAvroDeserializerConfig.AUTO_REGISTER_SCHEMAS,
+                environment.getProperty(KAFKA_PRODUCER_PREFIX + KafkaAvroDeserializerConfig.AUTO_REGISTER_SCHEMAS, Boolean.class, false));
+
+        // specific.avro.reader
+        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG,
+                environment.getProperty(KAFKA_PRODUCER_PREFIX + KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG,
+                        Boolean.class, SPECIFIC_AVRO_READER_CONFIG_VALUE));
+
+        // key.subject.name.strategy
+        props.put(AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY,
+                environment.getProperty(KAFKA_PRODUCER_PREFIX + AbstractKafkaAvroSerDeConfig.KEY_SUBJECT_NAME_STRATEGY,
+                        String.class, TopicRecordNameStrategy.class.getName()));
+
+        // value.subject.name.strategy
+        props.put(AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY,
+                environment.getProperty(KAFKA_PRODUCER_PREFIX + AbstractKafkaAvroSerDeConfig.VALUE_SUBJECT_NAME_STRATEGY,
+                        String.class, TopicRecordNameStrategy.class.getName()));
+
         return props;
     }
 
