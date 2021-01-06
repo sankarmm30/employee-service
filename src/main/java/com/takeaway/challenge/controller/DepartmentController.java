3f65175aa257d9adabd0c621dd39e91f397724ea
sankarmm30 <sankar.mm30@gmail.com>
2021-01-06 21:57:52 +0100

    Code refactoring: removed the usage of entity in department controller and related changes in test

--- src/main/java/com/takeaway/challenge/controller/DepartmentController.java
+++ src/main/java/com/takeaway/challenge/controller/DepartmentController.java
@@ -1,14 +1,10 @@
 package com.takeaway.challenge.controller;
 
-import com.takeaway.challenge.constant.ApiResponseMessage;
 import com.takeaway.challenge.dto.request.DepartmentRequestDto;
 import com.takeaway.challenge.dto.response.DepartmentResponseDto;
-import com.takeaway.challenge.model.DepartmentEntity;
 import com.takeaway.challenge.service.DepartmentService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +20,6 @@ import javax.validation.Valid;
 @Api(value = "Department controller", description = "This controller provides endpoint for managing the department of the company")
 public class DepartmentController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DepartmentController.class);
-
     private DepartmentService departmentService;
 
     public DepartmentController(DepartmentService departmentService) {
@@ -39,15 +33,8 @@ public class DepartmentController {
     @PostMapping(value = "/create", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DepartmentResponseDto> createDepartment(final @Valid @RequestBody DepartmentRequestDto departmentRequestDto) {
 
-        DepartmentEntity departmentEntity = this.departmentService.createDepartment(departmentRequestDto);
-
-        LOG.debug("Department has been created with id: {}", departmentEntity.getDepartId());
-
         return new ResponseEntity<>(
-                DepartmentResponseDto.builder()
-                        .departmentId(departmentEntity.getDepartId())
-                        .message(ApiResponseMessage.DEP_CREATE_MESSAGE.getValue())
-                        .build(),
+                this.departmentService.createDepartmentAndGetResponse(departmentRequestDto),
                 HttpStatus.CREATED);
     }
 }
--- src/main/java/com/takeaway/challenge/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/takeaway/challenge/service/impl/DepartmentServiceImpl.java
@@ -1,20 +1,29 @@
 package com.takeaway.challenge.service.impl;
 
+import com.takeaway.challenge.constant.ApiResponseMessage;
 import com.takeaway.challenge.dto.request.DepartmentRequestDto;
+import com.takeaway.challenge.dto.response.DepartmentResponseDto;
+import com.takeaway.challenge.exception.TakeAwayClientRuntimeException;
+import com.takeaway.challenge.exception.TakeAwayServerRuntimeException;
 import com.takeaway.challenge.factory.ValidationFactoryService;
 import com.takeaway.challenge.model.DepartmentEntity;
 import com.takeaway.challenge.repository.DepartmentEntityRepository;
 import com.takeaway.challenge.service.DepartmentService;
 import com.takeaway.challenge.util.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ConstraintViolationException;
 import java.util.Optional;
 
 @Service("departmentService")
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
 public class DepartmentServiceImpl implements DepartmentService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DepartmentServiceImpl.class);
 
     private DepartmentEntityRepository departmentEntityRepository;
     private ValidationFactoryService validationFactoryService;
@@ -30,11 +39,26 @@ public class DepartmentServiceImpl implements DepartmentService {
     @Transactional(propagation = Propagation.REQUIRED, rollbackFor = {Exception.class})
     public DepartmentEntity createDepartment(final DepartmentRequestDto departmentRequestDto) {
 
-        this.validationFactoryService.validObject(departmentRequestDto);
+        try {
 
-        return this.departmentEntityRepository.save(
-                DepartmentEntity.builder()
-                .name(departmentRequestDto.getName()).build());
+            // Validating the input parameter
+            this.validationFactoryService.validObject(departmentRequestDto);
+
+            // Saving the DepartmentEntity
+            return this.departmentEntityRepository.save(
+                    DepartmentEntity.builder()
+                            .name(departmentRequestDto.getName()).build());
+
+        } catch (TakeAwayClientRuntimeException | ConstraintViolationException exception) {
+
+            throw exception;
+
+        } catch (Exception exception) {
+
+            LOG.error("Exception while creating department",exception);
+
+            throw new TakeAwayServerRuntimeException("Unexpected error occurred", exception);
+        }
     }
 
     @Override
@@ -45,5 +69,18 @@ public class DepartmentServiceImpl implements DepartmentService {
         }
 
         return this.departmentEntityRepository.findById(departmentId);
+    }
+
+    @Override
+    public DepartmentResponseDto createDepartmentAndGetResponse(final DepartmentRequestDto departmentRequestDto) {
+
+        DepartmentEntity departmentEntity = this.createDepartment(departmentRequestDto);
+
+        LOG.debug("Department has been created with id: {}", departmentEntity.getDepartId());
+
+        return DepartmentResponseDto.builder()
+                .departmentId(departmentEntity.getDepartId())
+                .message(ApiResponseMessage.DEP_CREATE_MESSAGE.getValue())
+                .build();
     }
 }
